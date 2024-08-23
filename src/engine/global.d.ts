c2fe4ab44fc9507a2e161b070174922ae0602d71
Alexander Ivanov <ttecss@gmail.com>
2024-08-23 14:03:42 +0300

    Added more utility types

--- src/engine/global.d.ts
+++ src/engine/global.d.ts
@@ -7,5 +7,14 @@ type Instance = import('./scene/instance').default;
 type RenderingPass = import('./renderer').RenderingPass;
 type Engine = import('.').default;
 type Entities = import('./entities').default;
+
 type Tuple<T, N extends number, A extends T[] = []> = A['length'] extends N ? A : Tuple<T, N, [...A, T]>;
 type OptionalTuple<T, N extends number, A extends T[] = []> = A['length'] extends N ? A : A | OptionalTuple<T, N, [...A, T]>;
+type IfEquals<X, Y, A, B = never> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B;
+type WritableKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+}[keyof T];
+type ReadonlyKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, never, P>
+}[keyof T];
+type Filter<T, F> = { [K in keyof T as T[K] extends F ? K : never]: T[K] };
