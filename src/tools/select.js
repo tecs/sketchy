ec1aa2a9978911ed46f8fa4a427a422e86e631c0
Alexander Ivanov <ttecss@gmail.com>
2024-09-11 18:17:25 +0300

    Exit out of the entered instance by double-clicking only

--- src/tools/select.js
+++ src/tools/select.js
@@ -14,12 +14,6 @@ export default (engine) => {
     update() {},
     end(count = 1) {
       const { enteredInstance, selectedInstance, hoveredPointIndex, hoveredLineIndex, currentStep } = scene;
-      let clicked = scene.hoveredInstance;
-      let parent = clicked ? SubInstance.getParent(clicked) : undefined;
-      while (clicked && clicked !== enteredInstance && (parent?.instance ?? null) !== enteredInstance) {
-        clicked = parent?.instance ?? null;
-        parent = clicked ? SubInstance.getParent(clicked) : undefined;
-      }
 
       if (currentStep) {
         scene.setSelectedPoint(hoveredPointIndex);
@@ -27,17 +21,28 @@ export default (engine) => {
         return;
       }
 
-      if (count === 2 && clicked === selectedInstance) {
-        scene.setEnteredInstance(clicked);
+      let clicked = scene.hoveredInstance;
+      let parent = clicked ? SubInstance.getParent(clicked) : undefined;
+      while (clicked && clicked !== enteredInstance && (parent?.instance ?? null) !== enteredInstance) {
+        clicked = parent?.instance ?? null;
+        parent = clicked ? SubInstance.getParent(clicked) : undefined;
+      }
+
+      if (count === 1) {
+        if (selectedInstance && clicked === selectedInstance) return;
+        const clickedOwn = SubInstance.belongsTo(clicked, enteredInstance);
+
+        if (clicked === enteredInstance) scene.setSelectedInstance(null);
+        else if (clickedOwn) scene.setSelectedInstance(clicked);
+        else if (selectedInstance) scene.setSelectedInstance(null);
+
         return;
       }
-      if (selectedInstance && clicked === selectedInstance) return;
-      const clickedOwn = SubInstance.belongsTo(clicked, enteredInstance);
 
-      if (clicked === enteredInstance) scene.setSelectedInstance(null);
-      else if (clickedOwn) scene.setSelectedInstance(clicked);
-      else if (selectedInstance) scene.setSelectedInstance(null);
-      else scene.setEnteredInstance(enteredInstance ? SubInstance.getParent(enteredInstance)?.instance ?? null : null);
+      if (clicked && clicked === selectedInstance) scene.setEnteredInstance(clicked);
+      else if (clicked !== enteredInstance) {
+        scene.setEnteredInstance(enteredInstance ? SubInstance.getParent(enteredInstance)?.instance ?? null : null);
+      }
     },
     abort() {
       if (engine.tools.selected !== this || engine.scene.currentStep) return;
