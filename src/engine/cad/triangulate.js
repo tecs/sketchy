c821050fd9aa17e78062cfd2b897b06a9fc7d78d
Alexander Ivanov <ttecss@gmail.com>
2024-10-30 17:25:17 +0200

    Implemented triangulation holes

--- src/engine/cad/triangulate.js
+++ src/engine/cad/triangulate.js
@@ -1,8 +1,31 @@
 const { glMatrix: { equals }, vec2 } = glMatrix;
 
 /** @typedef {[i1: number, i2: number, angle: number]} Line */
+/**
+ * @typedef Loop
+ * @property {number[]} indices
+ * @property {Line[]} lines
+ */
+/**
+ * @typedef Triangulation
+ * @property {number[]} meshIndices
+ * @property {Loop} loop
+ * @property {Bucket[]} descendentBuckets
+ */
+/**
+ * @typedef Bucket
+ * @property {Triangulation[]} triangulations
+ * @property {number[]} outline
+ */
 
 const twoPI = Math.PI * 2;
+
+/**
+ * @param {vec2 | PlainVec2} v1
+ * @param {vec2 | PlainVec2} v2
+ * @returns {number}
+ */
+const cross = (v1, v2) => v1[0] * v2[1] - v1[1] * v2[0];
 
 /**
  * @param {number} i
@@ -16,22 +39,35 @@ const wrap = (i, l) => (i < 0 ? l + i : (i < l ? i : i - l));
  * @param {Readonly<ArrayLike<number>>} vertices
  * @param {Readonly<number[]>} sortedIndices
  * @param {number[]} loop
+ * @param {number[][]} holeLoops
  * @returns {number[]}
  */
-const earClip = (vertices, sortedIndices, loop) => {
+const earClip = (vertices, sortedIndices, loop, holeLoops) => {
   const polygons = /** @type {number[]} */ ([]);
 
-  /** @type {() => number[]} */
+  /** @type {() => { index: number, hole: number }[]} */
   const makeSortedIndices = () => sortedIndices.reduce((sorted, idx) => {
     const iLoop = loop.indexOf(idx);
-    if (iLoop > -1) sorted.push(iLoop);
+    if (iLoop > -1) {
+      sorted.push({ index: iLoop, hole: -1 });
+      return sorted;
+    }
+
+    for (let i = 0; i < holeLoops.length; ++i) {
+      const iHole = holeLoops[i].indexOf(idx);
+      if (iHole > -1) {
+        sorted.push({ index: iHole, hole: i });
+        return sorted;
+      }
+    }
+
     return sorted;
-  }, /** @type {number[]} */ ([]));
+  }, /** @type {ReturnType<typeof makeSortedIndices>} */ ([]));
 
   let iSortedIndices = makeSortedIndices();
 
   main: while (loop.length > 3) {
-    const iCurrent = iSortedIndices[0];
+    const iCurrent = iSortedIndices[0].index;
 
     const current = loop[iCurrent];
 
@@ -56,8 +92,10 @@ const earClip = (vertices, sortedIndices, loop) => {
     }
 
     for (let i = 1; i < iSortedIndices.length; ++i) {
-      const iTest = iSortedIndices[i];
-      const test = loop[iTest];
+      const hole = iSortedIndices[i].hole;
+      const isHole = hole > -1;
+      const iTest = iSortedIndices[i].index;
+      const test = isHole ? holeLoops[hole][iTest] : loop[iTest];
 
       const xt = vertices[test * 2];
       if (test === prev || test === next) continue;
@@ -73,6 +111,14 @@ const earClip = (vertices, sortedIndices, loop) => {
       const d = (xn - xp) * (yt - yp) - (yn - yp) * (xt - xp);
 
       if (d === 0 || (d < 0) === (s + t <= 0)) {
+        if (isHole) {
+          const [holeLoop] = holeLoops.splice(hole, 1);
+          loop.splice(iNext, 0, ...holeLoop.slice(iTest), ...holeLoop.slice(0, iTest + 1), current);
+          iSortedIndices = makeSortedIndices();
+
+          continue main;
+        }
+
         const length = Math.abs(iTest - iCurrent);
         const iFrom = iTest > iCurrent ? iCurrent : iTest;
 
@@ -83,6 +129,7 @@ const earClip = (vertices, sortedIndices, loop) => {
             vertices,
             sortedIndices,
             nonMonotoneLoop,
+            holeLoops,
           ),
         );
         loop.splice(iFrom + 1, length - 1);
@@ -105,10 +152,12 @@ const earClip = (vertices, sortedIndices, loop) => {
 /**
  * @param {Readonly<number[]>} vertices
  * @param {Readonly<number[]>} indices A-B-C-D-...
+ * @param {Readonly<number[]>[]} holesIndices (A-B-C-D-...)[]
  * @returns {number[]}
  */
-const triangulateFace = (vertices, indices) => {
-  const sortedIndices = [...indices].sort((a, b) => vertices[a * 2] - vertices[b * 2]);
+const triangulateFace = (vertices, indices, holesIndices = []) => {
+  const sortedIndices = [...indices].concat(holesIndices.flatMap(hole => [...hole]))
+    .sort((a, b) => vertices[a * 2] - vertices[b * 2]);
 
   const loop = [sortedIndices[0]];
 
@@ -118,7 +167,15 @@ const triangulateFace = (vertices, indices) => {
     ...indices.slice(0, indexSplit),
   );
 
-  return earClip(vertices, sortedIndices, loop);
+  const holeLoops = holesIndices.map(holeIndices => {
+    const holeStartIndex = sortedIndices.findIndex(idx => holeIndices.includes(idx));
+    return [
+      ...holeIndices.slice(holeStartIndex, holeIndices.length),
+      ...holeIndices.slice(0, holeStartIndex),
+    ];
+  });
+
+  return earClip(vertices, sortedIndices, loop, holeLoops);
 };
 
 /**
@@ -178,8 +235,8 @@ const intersect = (lines, vertices) => {
 
       vec2.subtract(diff, l2Coord, l1Coord);
 
-      const cross1 = l1Offset[0] * l2Offset[1] - l1Offset[1] * l2Offset[0];
-      const cross2 = diff[0] * l2Offset[1] - diff[1] * l2Offset[0];
+      const cross1 = cross(l1Offset, l2Offset);
+      const cross2 = cross(diff, l2Offset);
 
       // parallel
       if (equals(cross1, 0)) {
@@ -230,7 +287,7 @@ const intersect = (lines, vertices) => {
       }
 
       const l1scale = cross2 / cross1;
-      const l2scale = (diff[0] * l1Offset[1] - diff[1] * l1Offset[0]) / cross1;
+      const l2scale = cross(diff, l1Offset) / cross1;
 
       // check if intersects
       const touches1 = l1scale > 0 && l1scale < 1;
@@ -269,11 +326,12 @@ const intersect = (lines, vertices) => {
 
 /**
  * @param {Readonly<Line[]>} lines
- * @returns {number[][]}
+ * @param {boolean} outline
+ * @returns {Loop[]}
  */
-const findLoops = (lines) => {
+const findLoops = (lines, outline = false) => {
   const remaining = lines.slice();
-  const closedLoops = /** @type {number[][]} */ ([]);
+  const closedLoops = /** @type {Loop[]} */ ([]);
 
   while (true) {
     removeDangling(remaining);
@@ -282,7 +340,7 @@ const findLoops = (lines) => {
     if (!startingLine) break;
 
     const current = [startingLine];
-    let nextIndex = startingLine[1];
+    let nextIndex = startingLine[outline ? 0 : 1];
     let skipLine = /** @type {Line?} */ (null);
 
     while (true) {
@@ -333,7 +391,11 @@ const findLoops = (lines) => {
         const index = closed[closed.length - 1] === line[0] ? line[1] : line[0];
         if (index !== nextIndex) closed.push(index);
       }
-      closedLoops.push(closed);
+      closedLoops.push({ indices: closed, lines: current });
+
+      if (outline) {
+        return closedLoops;
+      }
 
       break;
     }
@@ -343,13 +405,137 @@ const findLoops = (lines) => {
 };
 
 /**
+ * @param {Triangulation[]} triangulations
+ * @param {PlainVec2[]} uniqueVertices
+ */
+const setHoles = (triangulations, uniqueVertices) => {
+  const flatVertices = uniqueVertices.flat();
+
+  const loopBuckets = /** @type {Bucket[]} */ ([]);
+  const remainingLoops = triangulations.slice();
+
+  // group touching loops into buckets
+  while (true) {
+    const initialLoop = remainingLoops.pop();
+    if (!initialLoop) break;
+
+    const bucket = /** @type {Bucket} */ ({ triangulations: [initialLoop], outline: [] });
+    const remainingIndices = initialLoop.loop.indices.slice();
+
+    while (true) {
+      const index = remainingIndices.pop();
+      if (index === undefined) break;
+
+      for (let i = remainingLoops.length - 1; i >= 0; --i) {
+        const otherLoop = remainingLoops[i];
+        if (!otherLoop.loop.indices.includes(index)) continue;
+
+        remainingLoops.splice(i, 1);
+        bucket.triangulations.push(otherLoop);
+        for (const otherIndex of otherLoop.loop.indices) {
+          if (otherIndex !== index && !remainingIndices.includes(otherIndex)) {
+            remainingIndices.push(otherIndex);
+          }
+        }
+      }
+    }
+
+    loopBuckets.push(bucket);
+  }
+
+  if (loopBuckets.length < 2) return;
+
+  const t = vec2.create();
+  const v1 = vec2.create();
+  const v2 = vec2.create();
+  const v3 = vec2.create();
+  const vv = vec2.create();
+  const vt = vec2.create();
+  let hasHoles = false;
+
+  // check if a random vertex from a bucket is inside any other bucket's loop
+  // if so, that first bucket's whole outline is inside the loop
+  for (let i = 0; i < loopBuckets.length - 1; ++i) {
+    for (let k = i + 1; k < loopBuckets.length; ++k) {
+      pairCheck:
+      for (const [bucket1, bucket2] of [[loopBuckets[i], loopBuckets[k]], [loopBuckets[k], loopBuckets[i]]]) {
+        vec2.set(t, ...uniqueVertices[bucket1.triangulations[0].loop.indices[0]]);
+        for (const { meshIndices: indices, descendentBuckets: descendents } of bucket2.triangulations) {
+          for (let ii = 0; ii < indices.length; ii += 3) {
+            vec2.set(v1, ...uniqueVertices[indices[ii]]);
+            vec2.set(v2, ...uniqueVertices[indices[ii + 1]]);
+            vec2.set(v3, ...uniqueVertices[indices[ii + 2]]);
+
+            vec2.subtract(vv, v2, v1);
+            vec2.subtract(vt, t, v1);
+            const crossSign1 = Math.sign(cross(vt, vv));
+
+            vec2.subtract(vv, v3, v2);
+            vec2.subtract(vt, t, v2);
+            const crossSign2 = Math.sign(cross(vt, vv));
+
+            if (crossSign1 !== crossSign2) continue;
+
+            vec2.subtract(vv, v1, v3);
+            vec2.subtract(vt, t, v3);
+            const crossSign3 = Math.sign(cross(vt, vv));
+
+            if (crossSign2 !== crossSign3) continue;
+
+            descendents.push(bucket1);
+            hasHoles = true;
+            break pairCheck;
+          }
+        }
+      }
+    }
+  }
+
+  if (!hasHoles) return;
+
+  // cleanup the buckets' descendents so they become a directed graph
+  for (const bucket1 of loopBuckets) {
+    const descendents = bucket1.triangulations.flatMap(loop => loop.descendentBuckets);
+    for (const bucket2 of loopBuckets) {
+      if (bucket1 === bucket2) continue;
+      for (const loop of bucket2.triangulations) {
+        if (loop.descendentBuckets.includes(bucket1)) {
+          for (const descendent of descendents) {
+            const index = loop.descendentBuckets.indexOf(descendent);
+            if (index !== -1) loop.descendentBuckets.splice(index, 1);
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  // re-triangulate loops with descendents, using their outlines as holes
+  for (const loop of triangulations) {
+    if (loop.descendentBuckets.length === 0) continue;
+
+    for (const bucket of loop.descendentBuckets) {
+      if (bucket.outline.length === 0) {
+        const uniqueLines = bucket.triangulations
+          .flatMap(({ loop: { lines } }) => lines)
+          .filter((v, i, a) => a.indexOf(v) === i);
+        bucket.outline = findLoops(uniqueLines, true).pop()?.indices ?? [];
+      }
+    }
+
+    const holes = loop.descendentBuckets.map(({ outline }) => outline);
+    loop.meshIndices = triangulateFace(flatVertices, loop.loop.indices, holes);
+  }
+};
+
+/**
  * @param {Readonly<number[]>} vertices
  * @param {Readonly<number[]>} lineIndices A-B,B-C,C-D,...
  * @returns {[indices: number[], vertices: number[]]}
  */
 export default (vertices, lineIndices) => {
   const lines = /** @type {Line[]} */ ([]);
-  const uniqueVertices = /** @type {PlainVec2[]} */ ([]);
+  const uniquePoints = /** @type {PlainVec2[]} */ ([]);
 
   // remove duplicate and zero-length lines and duplicate vertices
   for (let i = 1; i < lineIndices.length; i += 2) {
@@ -358,37 +544,37 @@ export default (vertices, lineIndices) => {
     const x2 = vertices[lineIndices[i] * 2];
     const y2 = vertices[lineIndices[i] * 2 + 1];
 
-    let i1 = uniqueVertices.findIndex(([x, y]) => equals(x, x1) && equals(y, y1));
+    let i1 = uniquePoints.findIndex(([x, y]) => equals(x, x1) && equals(y, y1));
     if (i1 === -1) {
-      i1 = uniqueVertices.length;
-      uniqueVertices.push([x1, y1]);
+      i1 = uniquePoints.length;
+      uniquePoints.push([x1, y1]);
     }
 
-    let i2 = uniqueVertices.findIndex(([x, y]) => equals(x, x2) && equals(y, y2));
+    let i2 = uniquePoints.findIndex(([x, y]) => equals(x, x2) && equals(y, y2));
     if (i2 === -1) {
-      i2 = uniqueVertices.length;
-      uniqueVertices.push([x2, y2]);
+      i2 = uniquePoints.length;
+      uniquePoints.push([x2, y2]);
     }
 
     if (i1 === i2) continue;
-    const [xx1, yy1] = uniqueVertices[i1];
-    const [xx2, yy2] = uniqueVertices[i2];
+    const [xx1, yy1] = uniquePoints[i1];
+    const [xx2, yy2] = uniquePoints[i2];
 
     const low = xx1 < xx2 || (xx1 === xx2 && yy1 > yy2) ? i1 : i2;
     const high = low === i1 ? i2 : i1;
     if (lines.some(line => line[0] === low && line[1] === high)) continue;
 
-    const x = uniqueVertices[high][0] - uniqueVertices[low][0];
-    const y = uniqueVertices[high][1] - uniqueVertices[low][1];
+    const x = uniquePoints[high][0] - uniquePoints[low][0];
+    const y = uniquePoints[high][1] - uniquePoints[low][1];
     const angle = Math.atan(y / x);
     lines.push([low, high, angle + (angle < 0 ? twoPI : 0)]);
   }
 
-  intersect(lines, uniqueVertices);
+  intersect(lines, uniquePoints);
 
   lines.sort(([i1,, a1], [i2,, a2]) => {
-    const [x1, y1] = uniqueVertices[i1];
-    const [x2, y2] = uniqueVertices[i2];
+    const [x1, y1] = uniquePoints[i1];
+    const [x2, y2] = uniquePoints[i2];
     if (x1 !== x2) return x1 - x2;
     if (y1 !== y2) return y2 - y1;
     if ((a1 > Math.PI && a2 > Math.PI) || (a1 < Math.PI && a2 < Math.PI)) return a1 - a2;
@@ -397,12 +583,18 @@ export default (vertices, lineIndices) => {
 
   const loops = findLoops(lines);
 
-  const meshIndices = /** @type {number[]} */ ([]);
+  const triangulations = /** @type {Triangulation[]} */ ([]);
 
-  const flatVertices = uniqueVertices.flat();
+  const flatVertices = uniquePoints.flat();
   for (const loop of loops) {
-    meshIndices.push(...triangulateFace(flatVertices, loop));
+    triangulations.push({
+      meshIndices: triangulateFace(flatVertices, loop.indices),
+      loop,
+      descendentBuckets: [],
+    });
   }
 
-  return [meshIndices, flatVertices];
+  setHoles(triangulations, uniquePoints);
+
+  return [triangulations.flatMap(({ meshIndices }) => meshIndices), flatVertices];
 };
