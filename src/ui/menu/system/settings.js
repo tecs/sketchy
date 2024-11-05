a67f85f091f16155330f7cc7447f5976383d6eae
Alexander Ivanov <ttecss@gmail.com>
2024-11-05 20:04:02 +0200

    Prettified the keyboard shortcuts settings page

--- src/ui/menu/system/settings.js
+++ src/ui/menu/system/settings.js
@@ -16,15 +16,35 @@ export default (engine, container) => {
     /** @type {Record<ReturnType<Engine["config"]["list"]>[number]["type"], string>} */
     const InputTypeMap = {
       int: 'number',
-      key: 'text',
+      key: 'hidden',
       toggle: 'checkbox',
     };
 
     /** @type {Record<string, import("../../lib/index.js").AnyUIParent>} */
     const tabMap = {};
 
-    /** @type {(el: HTMLElement) => boolean} */
-    const forceChange = (el) => el.dispatchEvent(new Event('change'));
+    /** @type {(el: HTMLElement, event: string) => boolean} */
+    const dispatchEvent = (el, event) => el.dispatchEvent(new Event(event));
+
+    /**
+     * @param {HTMLElement} el
+     * @param {string} keyCombo
+     */
+    const renderKeyCombo = (el, keyCombo) => {
+      const elements = /** @type {import("../../lib/element.js").Opts[2] & {}} */ ([]);
+
+      const combo = keyCombo !== '' ? keyCombo.split(' + ') : [];
+      for (const key of combo) {
+        if (key !== combo[0]) {
+          elements.push(['span', { innerText: '+' }]);
+        }
+        elements.push(['kbd', { innerText: key === ' ' ? 'space' : key }]);
+      }
+
+      if (elements.length === 0) elements.push(['em', { innerText: '<unset>' }]);
+
+      $(el, { innerHTML: '' }, elements);
+    };
 
     const settingsItems = engine.config.list().map(setting => {
       const el = $('div', { className: `setting${setting.value !== setting.defaultValue ? ' changed' : ''}` });
@@ -35,52 +55,84 @@ export default (engine, container) => {
       tabMap[category] ??= tabs.addTab(category);
       tabMap[category]?.$container({}, [el]);
 
+      const keyInput = $('div');
       const input = $('input', {
         type: InputTypeMap[setting.type],
         value: originalValue,
         checked: setting.type === 'toggle' && setting.value,
-        onkeydown({ key, ctrlKey, altKey, shiftKey }) {
-          if (setting.type !== 'key') return true;
-
-          // block non-printable keys
-          if (key.length !== 1) return false;
-
-          const combo = [key.toLowerCase()];
-          if (shiftKey) combo.unshift('shift');
-          if (altKey) combo.unshift('alt');
-          if (ctrlKey) combo.unshift('control');
-
-          input.value = combo.join(' + ');
-          forceChange(input);
-          input.blur();
-
-          return false;
-        },
         onchange() {
           if (setting.type === 'toggle') el.classList.toggle('changed', input.checked !== setting.defaultValue);
           else el.classList.toggle('changed', input.value !== defaultValue);
 
-          if (setting.type !== 'key' || input.value === '') return;
+          if (setting.type !== 'key') return;
+
+          dispatchEvent(keyInput, 'blur');
+
+          if (input.value === '') return;
 
           // make sure there are no shortcut conflicts
           for (const item of settingsItems) {
             if (item.setting !== setting && item.setting.type === 'key' && item.input.value === input.value) {
               item.input.value = '';
-              forceChange(item.input);
+              dispatchEvent(item.input, 'change');
             }
           }
         },
       });
 
+      /** @type {HTMLElement[]} */
+      const children = [input];
+      if (setting.type === 'key') {
+        children.push(keyInput);
+        renderKeyCombo(keyInput, originalValue);
+        $(keyInput, {
+          className: 'keyInput',
+          tabIndex: 0,
+          onclick() {
+            keyInput.focus();
+          },
+          onfocus() {
+            $(keyInput, { innerHTML: '' }, [['strong', { innerText: 'Press a key or key combination...' }]]);
+          },
+          onblur() {
+            if (document.activeElement === keyInput) {
+              keyInput.blur();
+            }
+            renderKeyCombo(keyInput, input.value);
+          },
+          onkeydown({ key, ctrlKey, altKey, shiftKey }) {
+            if (key === 'Escape') dispatchEvent(keyInput, 'blur');
+            else if (key === 'Delete') {
+              input.value = '';
+              dispatchEvent(input, 'change');
+            }
+
+            // block non-printable keys
+            if (key.length !== 1) return false;
+
+            const combo = [key.toLowerCase()];
+            if (shiftKey) combo.unshift('shift');
+            if (altKey) combo.unshift('alt');
+            if (ctrlKey) combo.unshift('control');
+
+            input.value = combo.join(' + ');
+            dispatchEvent(input, 'change');
+
+            return false;
+          },
+        });
+      }
+      const wrapper = $('div', { className: 'shortcutWrapper' }, children);
+
       $(el, {}, [
-        ['label', {}, [['span', { innerText: setting.name }], input]],
+        ['label', {}, [['span', { innerText: setting.name }], wrapper]],
         ['div', {
           className: 'button reset',
           innerText: '⟲',
           onclick: () => {
             input.value = defaultValue;
             input.checked = defaultValue === 'true';
-            forceChange(input);
+            dispatchEvent(input, 'change');
           },
         }],
       ]);
